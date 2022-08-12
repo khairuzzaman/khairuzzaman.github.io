a7e54123ef0f27b5577ffde009b0795893f1ecfb
Abu Zafor Khairuzzaman <azkhairuzzaman@gmail.com>
2022-08-13 00:39:59 +0600

    Updates

---
+++ precache-manifest.4485e47bdf89e8009d4f4cf2f241c0b1.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fe59960742b4fd8365ad1286e1e97ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3090b4aaa3f7f390d36",
+    "url": "/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "ab26c98b7785ab42d73e",
+    "url": "/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "37c11c42ea2bb21b134d",
+    "url": "/static/css/main.1a6005f5.chunk.css"
+  },
+  {
+    "revision": "36f13cccc590def0476e",
+    "url": "/static/js/2.ed6e47fa.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.ed6e47fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3090b4aaa3f7f390d36",
+    "url": "/static/js/3.87bb418f.chunk.js"
+  },
+  {
+    "revision": "ab26c98b7785ab42d73e",
+    "url": "/static/js/4.282081c5.chunk.js"
+  },
+  {
+    "revision": "37c11c42ea2bb21b134d",
+    "url": "/static/js/main.cdf3e603.chunk.js"
+  },
+  {
+    "revision": "2dde885fa1c19f520d10",
+    "url": "/static/js/runtime-main.96173119.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "1611755c29e42276ad2919f8c1faf3ab",
+    "url": "/static/media/bdskill.1611755c.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "628096eb74416324550686bcd9ff4de2",
+    "url": "/static/media/coursera.628096eb.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "033710b41fa04c900e9e4cecd767eb20",
+    "url": "/static/media/freecodecamp.033710b4.jpg"
+  },
+  {
+    "revision": "f0be2aa8d71afdf1a95001d01b061423",
+    "url": "/static/media/logo-ju.f0be2aa8.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "22f82d2f5b92806536a9a5528cab4d87",
+    "url": "/static/media/planetHR.22f82d2f.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "9f143b2196d6ed59df81217f96eff549",
+    "url": "/static/media/virtualbench.9f143b21.png"
+  }
+]);
